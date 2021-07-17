88d90551c60b54597103585459846a2e2cf0febd
keyaira91 <keyaira22@hotmail.com>
2021-07-17 01:21:30 -0400

    trying to set up click event

--- src/car.js
+++ src/car.js
@@ -4,12 +4,24 @@ import ReactDOM from "react-dom";
 
 
 class Car extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    var notReady = document.getElementById("myBtn")
+}
+
+  changeReady = (notReady) => {
+    this.setState({textDecoration: "line-trough"});
+  }
 
     render() {
       const {year, color, make, model, tag} = this.props.car;
       return (
-      <h2>I'm a {year} {color} {make}, {model} with a tag {tag}{" "}
-      <button type="button" className="btn btn-sm btn-danger">Ready</button></h2>);} 
+      <div>
+        <h2>{year} {color} {make}, {model} with tag # {tag}{" "}
+        <button type="button" id="myBtn" onClick={this.changeReady} 
+        className="btn btn-sm btn-danger">Ready</button></h2>
+      </div>)};
 };
 
 
