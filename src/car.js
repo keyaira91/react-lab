ac82daa50212618306a8e41d57ee347943e3723d
keyaira91 <keyaira22@hotmail.com>
2021-07-17 00:28:13 -0400

    added ready button

--- src/car.js
+++ src/car.js
@@ -8,11 +8,9 @@ class Car extends React.Component {
     render() {
       const {year, color, make, model, tag} = this.props.car;
       return (
-      <h2>
-        I'm a {year} {color} {make}, {model} with a tag {tag}</h2>
-        );
-      } 
-    }
+      <h2>I'm a {year} {color} {make}, {model} with a tag {tag}{" "}
+      <button type="button" className="btn btn-sm btn-danger">Ready</button></h2>);} 
+};
 
 
     export default Car; 
