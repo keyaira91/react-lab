ab74de94b07f496b11590977e34a4fa63cc83d38
keyaira91 <keyaira22@hotmail.com>
2021-07-16 23:45:27 -0400

    removed garage margin top

--- src/garage.js
+++ src/garage.js
@@ -10,7 +10,7 @@ class Garage extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="card">
+        <div className="card mb-3 mt-3">
           <div className="garage card-body">
             <h1>{this.props.garages.owner}'s Garage</h1>
            <p>{this.props.cars.map(this.doCarThingy)}</p>
