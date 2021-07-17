52351c2b839a5f6a6a0e7b2a6ef86434df9238ca
Keyaira <55303243+keyaira91@users.noreply.github.com>
2021-07-17 17:59:51 -0400

    working on ready button

--- src/car.js
+++ src/car.js
@@ -4,23 +4,26 @@ import ReactDOM from "react-dom";
 
 
 class Car extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-    var notReady = document.getElementById("myBtn")
-}
-
-  changeReady = (notReady) => {
-    this.setState({textDecoration: "line-trough"});
+  constructor(props) {
+    super(props)
+    this.state = {
+      ready: "Ready",
+      // deco: doCarThingy() ({textDecoration: "none"})
+    }
   }
+
+  notReady = () => {
+    this.setState({ready: "Not Ready", deco:({textDecoration: "line-trough"})})
+  }
+
 
     render() {
       const {year, color, make, model, tag} = this.props.car;
       return (
       <div>
-        <h2>{year} {color} {make}, {model} with tag # {tag}{" "}
-        <button type="button" id="myBtn" onClick={this.changeReady} 
-        className="btn btn-sm btn-danger">Ready</button></h2>
+        <h2 >{year} {color} {make}, {model} with tag # {tag}{" "}
+        <button type="button" id="myBtn" onClick={this.notReady}
+        className="btn btn-sm btn-danger">{this.state.ready}</button></h2>
       </div>)};
 };
 
