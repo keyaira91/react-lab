1103e734b2d3c4b3d2984eced3b1cbcb02e4a69a
keyaira91 <keyaira22@hotmail.com>
2021-07-19 12:38:44 -0400

    commented out react dom import

--- src/garage.js
+++ src/garage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import Car from "./car";
 import "./garage.css";
 class Garage extends React.Component {
