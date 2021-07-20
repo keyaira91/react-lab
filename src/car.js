08efd686aa94c74bb8f99de66d121d74c2c35c2b
keyaira91 <keyaira22@hotmail.com>
2021-07-19 20:10:15 -0400

    added Jeff's version

--- src/car.js
+++ src/car.js
@@ -37,5 +37,37 @@ class Car extends React.Component {
 };
 
 
+// class Car extends React.Component {
+//   state = {
+//     ready: true,
+//   };
+
+//   handleReady = () => {
+//     this.setState({ ready: !this.state.ready });
+//   };
+
+//   render() {
+//     const { make, model, year, color, tag } = this.props.car;
+
+//     return (
+//       <h2
+//         className={!this.state.ready ? "needswork" : " "}
+//         style={{ color: color }}
+//       >
+//         I am a {color} {year} {make} {model} with tag {tag}!
+//         <button
+//           onClick={this.handleReady}
+//           className={
+//             "btn btn-sm ml-2 " +
+//             (!this.state.ready ? "btn-success" : "btn-warning")
+//           }
+//         >
+//           {!this.state.ready ? "Ready" : "Needs Work"}
+//         </button>
+//       </h2>
+//     );
+//   }
+// }
+
     export default Car; 
 
