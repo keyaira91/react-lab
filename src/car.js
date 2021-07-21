acd7800b9493e75acc85bd0b387b52a1839ac26c
keyaira91 <keyaira22@hotmail.com>
2021-07-21 15:55:55 -0400

    added second btn

--- src/car.js
+++ src/car.js
@@ -9,31 +9,38 @@ class Car extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      ready: "Ready",
+      ready: "Take Out of Service",
       needsWork: false,
     }
   }
 
   notReady = () => {
-      this.setState({ ready: "Not Ready", needsWork: true });
-      if(this.state.ready === "Not Ready") {
+      this.setState({ ready: "Make Available", needsWork: true });
+      if(this.state.ready === "Make Available") {
         this.isReady()
       }
   }
 
   isReady = () => {
-      this.setState({ready: "Ready", needsWork: false});
+      this.setState({ready: "Take Out of Service", needsWork: false});
   }
 
     render() {
       const {year, color, make, model, tag} = this.props.car;
       return (
-      <div>
-        <h2 className={this.state.needsWork ? "needsWork" : " "}
-        >{year} {color} {make}, {model} with tag # {tag}{" "}
-        <button type="button" onClick={this.notReady}  
-        className="btn btn-sm btn-danger">{this.state.ready}</button></h2>
-      </div>)};
+      <div> 
+          <div className="float-left">
+        <h2 className={this.state.needsWork ? "needsWork" : " "}>
+           {year} {color} {make}, {model} with tag # {tag}{" "}</h2>
+         </div>
+         <div className="float-right">
+            <button type="button" onClick={this.notReady}  
+            className="btn btn-sm btn-danger">{this.state.ready}</button>{" "}
+            
+            <button type="button" className="btn btn-sm btn-primary">Drive</button>
+          </div>
+      </div>
+      )};
 };
 
 
